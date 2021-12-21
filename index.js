b1e6ffaa6a9bcd2a86453b13143f1213a5d24e0b
Victor Hugo Jimenez Torres <vjimenezt@psl.com.co>
2021-12-21 08:11:37 -0500

    vue setup

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var app = new Vue({ 
+
+new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue!'
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response))
     }
-});
+  })
